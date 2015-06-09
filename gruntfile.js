0e8daf0b8f7717519990b312696398cf3fdd2e32
felixyale <felix.yale@gmail.com>
2015-06-09 10:35:16 +0800

    gitignore

--- gruntfile.js
+++ gruntfile.js
@@ -3,29 +3,63 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 
 	grunt.config.init({
-	  useminPrepare: {
-	      html: 'index.html',
-	      options: {
-	        dest: 'dist'
-	      }
-	  },
-	  usemin:{
-	  	html:['dist/index.html']
-	  },
-	  copy:{
-	    html: {
-	    	src: './index.html', dest: 'dist/index.html'
-	    }
-	  }
+		useminPrepare: {
+			html: 'index.html',
+			options: {
+				dest: 'dist'
+			}
+		},
+		usemin:{
+			html:['dist/index.html']
+		},
+		copy:{
+			html: {
+				src: './index.html', dest: 'dist/index.html'
+			}
+		},
+		filerev: {
+			options: {
+				algorithm: 'md5',
+				summary: 'map.json',
+				length: 8
+			},
+			images: {
+				src: 'dist/assets/img/**/*.{jpg,jpeg,gif,png,webp}'
+			},
+			js: {
+				src: 'dist/assets/js/**'
+			},
+			css: {
+				src: 'dist/assets/css/**'
+			}
+		},
+		clean: {
+			release: {
+				src: 'dist/'
+			}
+		},
+		filerev_assets: {
+			dist: {
+				options: {
+					dest: 'dist/assets.json',
+					cwd: 'public/',
+					prettyPrint: true,
+					prefix: ''
+				}
+			}
+		}
 	});
 
 	grunt.registerTask('default',[
-		'copy:html',
+		'clean',
+		'copy',
 		'useminPrepare',
 		'concat',
 		'uglify',
-    'cssmin',
+		'cssmin',
+		'filerev',
+		'filerev_assets',
 		'usemin'
-    ]);
+	]);
 }
 
